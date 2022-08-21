09dd5fd72ce7d0ecca6ae10b0de9a4b8456fa5a0
BradleyMurrell <b.murrell89@gmail.com>
2022-08-21 13:38:11 +0000

    Add products views and templates

--- dog_fitness_and_nutrition/urls.py
+++ dog_fitness_and_nutrition/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('fitness_plans/', include('fitness_plans.urls')),
     path('nutrition_plans/', include('nutrition_plans.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

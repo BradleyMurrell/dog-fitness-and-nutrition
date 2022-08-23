3cb94a36677192e5e3eb9e9ab9599f8d947efc6c
BradleyMurrell <b.murrell89@gmail.com>
2022-08-23 18:44:00 +0000

    Add contact model to contact page

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    dogs_name = models.CharField(max_length=50, null=False, blank=False)
+    dogs_age = models.CharField(max_length=50, null=False, blank=False)
+    dogs_breed = models.CharField(max_length=50, null=False, blank=False)
+    message = models.TextField(max_length=2000, null=False, blank=False)

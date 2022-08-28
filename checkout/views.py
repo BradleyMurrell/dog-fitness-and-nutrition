ca2c946b1c645d18071505561f482a1fc927881e
BradleyMurrell <b.murrell89@gmail.com>
2022-08-28 09:20:02 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LJZVaBTdYuyDtvfLAwRLcNVcbwiOQWSOddfmobYWrmxG7wIykD3pLSaxT2zU62JPJFxKVNAYkYnK3Cugshy1oy000fbeaTynQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

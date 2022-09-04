9b2ded9e47cbe7404cd0401696e650053ee64c1e
BradleyMurrell <b.murrell89@gmail.com>
2022-09-04 13:04:04 +0000

    Set up fitness plan contact form

---
+++ fitness_plans/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+class SessionForm(forms.Form):
+
+    SESSION = (
+        ('try', 'Try'),
+        ('term', 'Term'),
+        ('single session', 'Single session'),
+    )
+
+    your_name = forms.CharField(max_length=255)
+    dogs_name = forms.CharField(max_length=255)
+    dogs_age = forms.CharField(max_length=255)
+    dogs_breed = forms.CharField(max_length=255)
+    sessions = forms.ChoiceField(choices=SESSION)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
--- fitness_plans/views.py
+++ fitness_plans/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
-# Create your views here.
+from .forms import SessionForm
 
 def fitness_plans(request):
     """A view to return the fitness plans page"""
@@ -8,6 +10,32 @@ def fitness_plans(request):
     return render(request, 'fitness_plans/fitness_plans.html')
 
 def fitness_plan_contact(request):
-    """A view to return the fitness plan contact page"""
+    if request.method == 'POST':
+        form = SessionForm(request.POST)
 
-    return render(request, 'fitness_plans/fitness_plan_contact.html')
+        if form.is_valid():
+            your_name = form.cleaned_data['your_name']
+            dogs_name = form.cleaned_data['dogs_name']
+            dogs_age = form.cleaned_data['dogs_age']
+            dogs_breed = form.cleaned_data['dogs_breed']
+            sessions = form.cleaned_data['sessions']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            html = render_to_string('fitness_plans/email/fitness_email.html', {
+                'your_name': your_name,
+                'dogs_name': dogs_name,
+                'dogs_age': dogs_age,
+                'dogs_breed': dogs_breed,
+                'sessions': sessions,
+                'email': email,
+                'message': message
+            })
+
+            send_mail('Form subject', 'Form message', 'DEFAULT_FROM_EMAIL', ['dog.fitness.nutrition@gmail.com'], html_message=html)
+
+            return render(request, 'contact/contact_success.html')
+
+    form = SessionForm()
+    context = {'form': form}
+    return render(request, 'fitness_plans/fitness_plan_contact.html', context)

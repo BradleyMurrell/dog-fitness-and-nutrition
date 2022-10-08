77e8d6dcdf3121e96a12d92fc21be623c7f1b14b
BradleyMurrell <b.murrell89@gmail.com>
2022-10-08 12:00:54 +0000

    Add 404 page

---
+++ dog_fitness_and_nutrition/views.py
@@ -0,0 +1,7 @@
+""" Imports """
+from django.shortcuts import render
+
+
+def handler404(request):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)

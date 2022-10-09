7fe8c65717aede229fdefeef851a2c25847d54c4
BradleyMurrell <b.murrell89@gmail.com>
2022-10-09 13:46:58 +0000

    Tidy up python files in dog fitness and nutrition app

--- dog_fitness_and_nutrition/asgi.py
+++ dog_fitness_and_nutrition/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dog_fitness_and_nutrition.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'dog_fitness_and_nutrition.settings'
+)
 
 application = get_asgi_application()
--- dog_fitness_and_nutrition/wsgi.py
+++ dog_fitness_and_nutrition/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dog_fitness_and_nutrition.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'dog_fitness_and_nutrition.settings'
+)
 
 application = get_wsgi_application()

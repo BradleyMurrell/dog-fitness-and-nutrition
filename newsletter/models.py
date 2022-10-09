94ea161dcebf521f77f51c2119bd54bcb51a5d67
BradleyMurrell <b.murrell89@gmail.com>
2022-10-09 19:58:57 +0000

    Remove str from newsletter models

--- newsletter/models.py
+++ newsletter/models.py
@@ -7,14 +7,8 @@ class Subscribers(models.Model):
     email = models.EmailField(null=True)
     date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.email
-
 
 class MailMessage(models.Model):
     """ Model for sending messages to subscribers """
     title = models.CharField(max_length=100, null=True)
     message = models.TextField(null=True)
-
-    def __str__(self):
-        return self.title
